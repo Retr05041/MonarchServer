0a4cda1e6a1480938e60c21cf6bef5f2c38a0eec
Retr05041 <parker@cranfield.ca>
2024-05-02 18:30:45 -0700

    Lexer1.0.0

--- cmd/parser/astgen/astgen.go
+++ cmd/parser/astgen/astgen.go
@@ -1,1 +1,63 @@
 package astgen
+
+import (
+	"MonarchServer/cmd/parser/tokenizer"
+	"bytes"
+)
+
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+type Prefix struct {
+	Token tokenizer.Token
+	Value string
+}
+
+func (p *Prefix) TokenLiteral() string { return p.Token.Literal }
+func (p *Prefix) String() string       { return p.Value }
+
+type Command struct {
+	Token  tokenizer.Token
+	Value  string
+	Params []string
+}
+
+func (c *Command) TokenLiteral() string { return c.Token.Literal }
+func (c *Command) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(" " + c.Value + " ")
+	for i, param := range c.Params {
+		out.WriteString(param)
+		if i != len(c.Params)-1 {
+			out.WriteString(" ")
+		}
+	}
+
+	return out.String()
+}
+
+// Root Node for every AST
+type AST struct {
+	Nodes []Node
+}
+
+func (a *AST) TokenLiteral() string {
+	if len(a.Nodes) > 0 {
+		return a.Nodes[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (a *AST) String() string {
+	var out bytes.Buffer
+
+	for _, s := range a.Nodes{
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
--- cmd/parser/astgen/astgen_test.go
+++ cmd/parser/astgen/astgen_test.go
@@ -1,1 +1,26 @@
 package astgen
+
+import (
+	"MonarchServer/cmd/parser/tokenizer"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	ast := &AST{
+		Nodes: []Node{
+            &Prefix{
+                Token: tokenizer.Token{Type: tokenizer.WORD, Literal: "TestSource"},
+                Value: "TestSource",
+            },
+            &Command{
+                Token: tokenizer.Token{Type: tokenizer.WORD, Literal: "PASS"},
+                Value: "PASS",
+                Params: []string{"supersecretpassword"},
+            },
+		},
+	}
+
+	if ast.String() != "TestSource PASS supersecretpassword" {
+		t.Errorf("program.String() wrong. got=%q", ast.String())
+	}
+}
